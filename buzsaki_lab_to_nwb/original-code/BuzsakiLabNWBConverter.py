c98e146c3f569d2921fdc87dedb2f7b126de4f17
Cody Baker <cbaker9@nd.edu>
2020-07-29 17:15:43 -0400

    cleaning old files; moved primary class definitions to nwb-conversion-tools

--- buzsaki_lab_to_nwb/original-code/BuzsakiLabNWBConverter.py
+++ buzsaki_lab_to_nwb/original-code/BuzsakiLabNWBConverter.py
@@ -1,8 +1,7 @@
 
-from NWBConverter import NWBConverter
-from BaseDataInterface import NeuroscopeRecordingInterface
+from nwb_conversion_tools import NWBConverter, NeuroscopeDataInterface
 
 class BuzsakiLabNWBConverter(NWBConverter):
-    data_interface_classes = {'neuroscope': NeuroscopeRecordingInterface}
+    data_interface_classes = {'neuroscope': NeuroscopeDataInterface.NeuroscopeRecordingInterface}
     
     
--- buzsaki_lab_to_nwb/original-code/old_run_convert_yuta.py
+++ buzsaki_lab_to_nwb/original-code/old_run_convert_yuta.py
@@ -1,6 +1,6 @@
 
 
-import convert_yuta
+import old_convert_yuta as convert_yuta
 
 convert_yuta.yuta2nwb(session_path='D:/BuzsakiData/SenzaiY/YutaMouse41/YutaMouse41-150903',
                                       include_spike_waveforms=True, stub=True)
