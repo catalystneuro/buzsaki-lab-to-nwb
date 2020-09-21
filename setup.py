53e2e94d7b7eea7442edcc2ba82761f1679d8f1f
Cody Baker <cbaker9@nd.edu>
2020-09-21 14:29:25 -0400

    updated setup to work on new enviroments

--- setup.py
+++ setup.py
@@ -14,6 +14,6 @@ setup(name='buzsaki_lab_to_nwb',
       email='ben.dichter@gmail.com',
       packages=find_packages(),
       include_package_data=True,
-      install_requires=['matplotlib', 'cycler', 'scipy', 'numpy', 'jupyter',
+      install_requires=['matplotlib', 'cycler', 'scipy', 'numpy', 'jupyter', 'xlrd',
                         'h5py', 'pynwb', 'spikeextractors', 'lxml', 'typing', 'nwbn-conversion-tools'],
       )
