78601c14beccdfd1e866ea528ac1232dfa46d299
Cody Baker <cbaker9@nd.edu>
2020-09-29 14:53:17 -0400

    added and renamed parent directories

---
+++ buzsaki_lab_to_nwb/yuta_code/convert_yuta.py
@@ -0,0 +1,68 @@
+"""Authors: Cody Baker and Ben Dichter."""
+from buzsaki_lab_to_nwb import YutaNWBConverter
+
+# TODO: add pathlib
+import os
+
+# List of folder paths to iterate over
+convert_sessions = ["D:/BuzsakiData/SenzaiY/YutaMouse41/YutaMouse41-150903",
+                    "D:/BuzsakiData/SenzaiY/YutaMouse20/YutaMouse20-140225",
+                    "D:/BuzsakiData/SenzaiY/YutaMouse55/YutaMouse55-160907"]
+
+session_descriptions = ["mouse in open exploration and theta maze",
+                        "mouse in open exploration and theta maze",
+                        "mouse in open exploration and theta maze"]
+
+# Session specific info
+n_sessions = len(convert_sessions)
+session_specific_metadata = [{}] * n_sessions
+for j in range(n_sessions):
+    session_specific_metadata[j]['NWBFile'] = {}
+    session_specific_metadata[j]['NWBFile'].update({'session_description': session_descriptions[j]})
+# session_specific_metadata[0]['NWBFile'].update({'related_publications': 'DOI:10.1016/j.neuron.2016.12.011'})
+
+for j, session in enumerate(convert_sessions):
+    # TODO: replace with pathlib
+    session_name = os.path.split(session)[1]
+
+    input_file_schema = YutaNWBConverter.get_input_schema()
+
+    # construct input_args dict according to input schema
+    input_args = {
+        'NeuroscopeRecording': {'file_path': os.path.join(session, session_name) + ".dat"},
+        'NeuroscopeSorting': {'folder_path': session,
+                              'keep_mua_units': False},
+        'YutaPosition': {'folder_path': session},
+        'YutaLFP': {'folder_path': session},
+        'YutaBehavior': {'folder_path': session}
+    }
+
+    yuta_converter = YutaNWBConverter(**input_args)
+
+    expt_json_schema = yuta_converter.get_metadata_schema()
+
+    # expt_json_schema does not indicate device linking in ElectrodeGroup.
+    # Also out of place 'type' in property levels?
+
+    # construct metadata_dict according to expt_json_schema
+    metadata = yuta_converter.get_metadata()
+
+    # TODO: better way to nest smart dictionary updates?
+    for key1, val1 in session_specific_metadata[j].items():
+        if type(val1) is dict:
+            for key2, val2 in val1.items():
+                metadata[key1].update({key2: val2})
+        else:
+            metadata.update({key1: val1})
+
+    # Yuta specific info
+    metadata['NWBFile'].update({'experimenter': 'Yuta Senzai'})
+
+    metadata[yuta_converter.get_recording_type()]['Ecephys']['Device'][0].update({'name': 'implant'})
+
+    for electrode_group_metadata in metadata[yuta_converter.get_recording_type()]['Ecephys']['ElectrodeGroup']:
+        electrode_group_metadata.update({'location': 'unknown'})
+        electrode_group_metadata.update({'device_name': 'implant'})
+
+    nwbfile_path = session + "_new_converter.nwb"
+    yuta_converter.run_conversion(nwbfile_path, metadata, stub_test=True)
---
+++ buzsaki_lab_to_nwb/yuta_code/yutalfpdatainterface.py
@@ -0,0 +1,91 @@
+"""Authors: Cody Baker and Ben Dichter."""
+from nwb_conversion_tools.utils import get_base_schema, get_schema_from_hdmf_class
+from nwb_conversion_tools.basedatainterface import BaseDataInterface
+from pynwb import NWBFile, TimeSeries
+from pynwb.misc import DecompositionSeries
+import os
+import numpy as np
+
+from ..band_analysis import filter_lfp, hilbert_lfp
+from ..neuroscope import read_lfp, write_lfp, write_spike_waveforms, check_module
+
+
+class YutaLFPInterface(BaseDataInterface):
+
+    @classmethod
+    def get_input_schema(cls):
+        return {}
+
+
+    def __init__(self, **input_args):
+        super().__init__(**input_args)
+
+    def get_metadata_schema(self):
+        metadata_schema = get_base_schema()
+
+        # ideally most of this be automatically determined from pynwb docvals
+        metadata_schema['properties']['TimeSeries'] = get_schema_from_hdmf_class(TimeSeries)
+        metadata_schema['properties']['DecompositionSeries'] = get_schema_from_hdmf_class(DecompositionSeries)
+        required_fields = ['TimeSeries', 'DecompositionSeries']
+        for field in required_fields:
+            metadata_schema['required'].append(field)
+
+        return metadata_schema
+
+    def convert_data(self, nwbfile: NWBFile, metadata_dict: dict, stub_test: bool = False):
+        session_path = self.input_args['folder_path']
+        # TODO: check/enforce format?
+        all_shank_channels = metadata_dict['all_shank_channels']
+        special_electrode_dict = metadata_dict['special_electrodes']
+        lfp_channel = metadata_dict['lfp_channel']
+        lfp_sampling_rate = metadata_dict['lfp_sampling_rate']
+        spikes_nsamples = metadata_dict['spikes_nsamples']
+        shank_channels = metadata_dict['shank_channels']
+
+        subject_path, session_id = os.path.split(session_path)
+
+        _, all_channels_lfp_data = read_lfp(session_path, stub=stub_test)
+        lfp_data = all_channels_lfp_data[:, all_shank_channels]
+        lfp_ts = write_lfp(nwbfile, lfp_data, lfp_sampling_rate,
+                           name=metadata_dict['lfp']['name'],
+                           description=metadata_dict['lfp']['description'],
+                           electrode_inds=None)
+
+        # TODO: error checking on format?
+        for special_electrode in special_electrode_dict:
+            ts = TimeSeries(name=special_electrode['name'],
+                            description=special_electrode['description'],
+                            data=all_channels_lfp_data[:, special_electrode['channel']],
+                            rate=lfp_sampling_rate, unit='V', resolution=np.nan)
+            nwbfile.add_acquisition(ts)
+
+        # TODO: discuss/consider more robust checking well prior to this
+        # when missing experimental sheets for a subject, the lfp_channel cannot be determined(?)
+        # which causes uninformative downstream errors at this step because lfp_channel is None
+        # (get_reference_electrode does throw a warning, though)
+        if lfp_channel is not None:
+            all_lfp_phases = []
+            for passband in ('theta', 'gamma'):
+                lfp_fft = filter_lfp(lfp_data[:, all_shank_channels == lfp_channel].ravel(),
+                                     lfp_sampling_rate,
+                                     passband=passband)
+                lfp_phase, _ = hilbert_lfp(lfp_fft)
+                all_lfp_phases.append(lfp_phase[:, np.newaxis])
+            decomp_series_data = np.dstack(all_lfp_phases)
+
+            # TODO: should units or metrics be metadata?
+            decomp_series = DecompositionSeries(name=metadata_dict['lfp_decomposition']['name'],
+                                                description=metadata_dict['lfp_decomposition']['description'],
+                                                data=decomp_series_data,
+                                                rate=lfp_sampling_rate,
+                                                source_timeseries=lfp_ts,
+                                                metric='phase', unit='radians')
+            # TODO: the band limits should be extracted from parse_passband in band_analysis?
+            decomp_series.add_band(band_name='theta', band_limits=(4, 10))
+            decomp_series.add_band(band_name='gamma', band_limits=(30, 80))
+
+            check_module(nwbfile, 'ecephys',
+                         'contains processed extracellular electrophysiology data').add_data_interface(decomp_series)
+
+        write_spike_waveforms(nwbfile, session_path, spikes_nsamples=spikes_nsamples, shank_channels=shank_channels,
+                              stub_test=stub_test)
