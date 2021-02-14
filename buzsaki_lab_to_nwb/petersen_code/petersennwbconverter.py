4307474e04aaf78f7f1697292ebd24e27e53cb7a
Cody Baker <cbaker9@nd.edu>
2021-02-14 17:44:56 -0500

    removed other subject info that isn't always there

--- buzsaki_lab_to_nwb/petersen_code/petersennwbconverter.py
+++ buzsaki_lab_to_nwb/petersen_code/petersennwbconverter.py
@@ -67,8 +67,8 @@ class PetersenNWBConverter(NWBConverter):
             Subject=dict(
                 subject_id=session_id[6:10],
                 species="Rattus norvegicus domestica - Long Evans",
-                genotype=session_info['general']['geneticLine'][0][0][0][0],
-                sex=session_info['general']['sex'][0][0][0][0],
+                genotype="Wild type",
+                sex="Male",
                 age="3-6 months"
             )
         )
