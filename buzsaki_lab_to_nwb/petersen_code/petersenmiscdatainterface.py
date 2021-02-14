9857e270c8e672b1a41a3756c236731f4ea4939a
Cody Baker <cbaker9@nd.edu>
2021-02-14 18:45:51 -0500

    adding condition for duplicate Take files

--- buzsaki_lab_to_nwb/petersen_code/petersenmiscdatainterface.py
+++ buzsaki_lab_to_nwb/petersen_code/petersenmiscdatainterface.py
@@ -16,6 +16,7 @@ from ..neuroscope import check_module
 # Include eeg & ripple data
 # Add various raw acquisition sources with metadata from intan rhd file
 # Not all foldesr had an animal.mat file for pos info
+# Fix issues with duplicate Take files in some sessions
 
 class PetersenMiscInterface(BaseDataInterface):
     """Primary data interface for miscellaneous aspects of the PetersenP dataset."""
@@ -36,59 +37,59 @@ class PetersenMiscInterface(BaseDataInterface):
         # Trials
         take_file_paths = [x for x in session_path.iterdir() if "Take" in x.name]
         # Some sessions had duplicate/non-corresponding Take files
-        assert len(take_file_paths) == 1, "More than one Take file found in path! Please remove excess or update code."
-        take_file_path = take_file_paths[0]
-        take_file = pd.read_csv(take_file_path, header=5)
-        take_file_time_name = [x for x in take_file if "Time" in x][0]  # Can be either 'Time' or 'Time (Seconds)'
-        take_frame_to_time = {x: y for x, y in zip(take_file['Frame'], take_file[take_file_time_name])}
+        if len(take_file_paths) == 1:
+            take_file_path = take_file_paths[0]
+            take_file = pd.read_csv(take_file_path, header=5)
+            take_file_time_name = [x for x in take_file if "Time" in x][0]  # Can be either 'Time' or 'Time (Seconds)'
+            take_frame_to_time = {x: y for x, y in zip(take_file['Frame'], take_file[take_file_time_name])}
 
-        trial_info = loadmat(str(session_path / f"{session_id}.trials.behavior.mat"))['trials']
-        trial_start_frames = trial_info['start'][0][0]
-        n_trials = len(trial_start_frames)
-        trial_end_frames = trial_info['end'][0][0]
-        trial_stat = trial_info['stat'][0][0]
-        trial_stat_labels = [x[0][0] for x in trial_info['labels'][0][0]]
-        trial_temperature = trial_info['temperature'][0][0]
-        cooling_info = trial_info['cooling'][0][0]
-        cooling_map = dict({0: "Cooling off", 1: "Pre-Cooling", 2: "Cooling on", 3: "Post-Cooling"})
-        trial_error = trial_info['error'][0][0]
-        error_trials = np.array([False]*n_trials)
-        error_trials[np.array(trial_error).astype(int)-1] = True  # -1 from Matlab indexing
+            trial_info = loadmat(str(session_path / f"{session_id}.trials.behavior.mat"))['trials']
+            trial_start_frames = trial_info['start'][0][0]
+            n_trials = len(trial_start_frames)
+            trial_end_frames = trial_info['end'][0][0]
+            trial_stat = trial_info['stat'][0][0]
+            trial_stat_labels = [x[0][0] for x in trial_info['labels'][0][0]]
+            trial_temperature = trial_info['temperature'][0][0]
+            cooling_info = trial_info['cooling'][0][0]
+            cooling_map = dict({0: "Cooling off", 1: "Pre-Cooling", 2: "Cooling on", 3: "Post-Cooling"})
+            trial_error = trial_info['error'][0][0]
+            error_trials = np.array([False]*n_trials)
+            error_trials[np.array(trial_error).astype(int)-1] = True  # -1 from Matlab indexing
 
-        trial_starts = []
-        trial_ends = []
-        trial_condition = []
-        for k in range(n_trials):
-            trial_starts.append(take_frame_to_time[trial_start_frames[k]])
-            trial_ends.append(take_frame_to_time[trial_end_frames[k]])
-            nwbfile.add_trial(start_time=trial_starts[k], stop_time=trial_ends[k])
-            trial_condition.append(trial_stat_labels[int(trial_stat[k])-1])
-
-        nwbfile.add_trial_column(
-            name='condition',
-            description="Whether the maze condition was left or right.",
-            data=trial_condition
-        )
-        nwbfile.add_trial_column(
-            name='error',
-            description="Whether the subject made a mistake.",
-            data=error_trials
-        )
-        nwbfile.add_trial_column(
-            name='temperature',
-            description="Average brain temperature for the trial.",
-            data=trial_temperature
-        )
-
-        if len(cooling_info) == n_trials:  # some sessions had incomplete cooling info
-            trial_cooling = []
+            trial_starts = []
+            trial_ends = []
+            trial_condition = []
             for k in range(n_trials):
-                trial_cooling.append(cooling_map[int(cooling_info[k])])
+                trial_starts.append(take_frame_to_time[trial_start_frames[k]])
+                trial_ends.append(take_frame_to_time[trial_end_frames[k]])
+                nwbfile.add_trial(start_time=trial_starts[k], stop_time=trial_ends[k])
+                trial_condition.append(trial_stat_labels[int(trial_stat[k])-1])
+
             nwbfile.add_trial_column(
-                name='cooling state',
-                description="The labeled cooling state of the subject during the trial.",
-                data=trial_cooling
+                name='condition',
+                description="Whether the maze condition was left or right.",
+                data=trial_condition
             )
+            nwbfile.add_trial_column(
+                name='error',
+                description="Whether the subject made a mistake.",
+                data=error_trials
+            )
+            nwbfile.add_trial_column(
+                name='temperature',
+                description="Average brain temperature for the trial.",
+                data=trial_temperature
+            )
+
+            if len(cooling_info) == n_trials:  # some sessions had incomplete cooling info
+                trial_cooling = []
+                for k in range(n_trials):
+                    trial_cooling.append(cooling_map[int(cooling_info[k])])
+                nwbfile.add_trial_column(
+                    name='cooling state',
+                    description="The labeled cooling state of the subject during the trial.",
+                    data=trial_cooling
+                )
 
         # Position
         animal_file_path = session_path / "animal.mat"
