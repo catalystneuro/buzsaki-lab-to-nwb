e989bb390591df7a3292bda004516302b712c263
h-mayorquin <h.mayorquin@gmail.com>
2021-11-17 07:54:50 +0000

    added raw data files when they exists to conversion script

--- buzsaki_lab_to_nwb/tingley_code_septal/convert_tingley_septal.py
+++ buzsaki_lab_to_nwb/tingley_code_septal/convert_tingley_septal.py
@@ -7,34 +7,51 @@ conversion_factor = 0.195  # Intan
 data_path = Path("/shared/catalystneuro/Buzsaki/TingleyD/")
 nwb_output_path = Path("/shared/catalystneuro/Buzsaki/TingleyD/nwbfiles")
 
-subject_test = "DT5"  # Just for testing
+subject_list = ["DT2", "DT5", "DT7", "DT8", "D79"]
+# subject_list = ['DT5']
+
+invalid_session = [
+    "20170411_1296um_1152um_170411_113418",  # No data
+    "z_Intruder_test_160304_152951",  # Test
+    "z_USV_test_3612um_1360um_20160307_160307_202140",  # Test
+    "z_novel_cage_test_160227_165229",  # Test
+]
 
 session_list = [
     session
     for subject in data_path.iterdir()
-    if subject.is_dir() and subject.name == subject_test
+    if subject.is_dir() and subject.name in subject_list
     for session in subject.iterdir()
-    if session.is_dir()
+    if session.is_dir() and session.name not in invalid_session
 ]
 
 
 for session_path in session_list:
     session_id = session_path.name
-    lfp_file_path = str(session_path / f"{session_path.name}.lfp")
-    nwbfile_path = str((nwb_output_path / f"{session_id}_stub.nwb"))
+    lfp_file_path = session_path / f"{session_path.name}.lfp"
+    raw_file_path = session_path / f"{session_id}.dat"
+    nwbfile_path = nwb_output_path / f"{session_id}_stub.nwb"
 
     source_data = dict(
-        NeuroscopeLFP=dict(file_path=lfp_file_path, gain=conversion_factor),
+        NeuroscopeLFP=dict(file_path=str(lfp_file_path), gain=conversion_factor),
     )
-
     conversion_options = dict(NeuroscopeLFP=dict(stub_test=stub_test))
 
-    converter = TingleySeptalNWBConverter(source_data)
+    if raw_file_path.is_file():
+        print("Here")
+        source_data.update(
+            NeuroscopeRecording=dict(
+                file_path=str(raw_file_path),
+                gain=conversion_factor,
+            )
+        )
+    conversion_options.update(NeuroscopeRecording=dict(stub_test=stub_test))
+
     metadata = None
     # metadata = converter.get_metadata()
     # metadata["Subject"].update(weight=f"{subject_weight[subject_name]}g")
     converter.run_conversion(
-        nwbfile_path=nwbfile_path,
+        nwbfile_path=str(nwbfile_path),
         metadata=metadata,
         conversion_options=conversion_options,
         overwrite=True,
