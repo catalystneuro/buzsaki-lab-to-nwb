3f9e64be6a71a839e442e9e84e4f9580cb55109d
h-mayorquin <h.mayorquin@gmail.com>
2022-01-15 09:00:03 +0100

    added end of the session trial reading

--- buzsaki_lab_to_nwb/tingley_code_septal/tingleyseptalbehaviorinterface.py
+++ buzsaki_lab_to_nwb/tingley_code_septal/tingleyseptalbehaviorinterface.py
@@ -10,6 +10,7 @@ from pynwb.behavior import SpatialSeries, Position, CompassDirection
 from nwb_conversion_tools.basedatainterface import BaseDataInterface
 from nwb_conversion_tools.utils.conversion_tools import get_module
 from nwb_conversion_tools.utils.json_schema import FolderPathType
+from spikeextractors import NeuroscopeRecordingExtractor
 
 from .tingleyseptal_utils import read_matlab_file
 
@@ -45,14 +46,13 @@ class TingleySeptalBehaviorInterface(BaseDataInterface):
         trial_list = events["trials"]
         direction_list = [trial.get("direction", "") for trial in trial_list]
         trial_type_list = [trial.get("type", "") for trial in trial_list]
-        
+
         if not all([direction == "" for direction in direction_list]):
             nwbfile.add_trial_column(name="direction", description="direction of the trial", data=direction_list)
 
         if not all([trial_type == "" for trial_type in trial_type_list]):
             nwbfile.add_trial_column(name="trial_type", description="type of trial", data=trial_type_list)
 
-                    
         # Position
         module_name = "behavior"
         module_description = "Contains behavioral data concerning position."
@@ -165,13 +165,23 @@ class TingleySeptalBehaviorInterface(BaseDataInterface):
             [table.add_row(**row) for row in sorted(data, key=lambda x: x["start_time"])]
             processing_module.add(table)
 
-            
         # Add epochs
+        lfp_file_path = session_path / f"{session_path.name}.lfp"
+        raw_file_path = session_path / f"{session_id}.dat"
+        if raw_file_path.is_file():
+            recorder = NeuroscopeRecordingExtractor(file_path=raw_file_path)
+        else:
+            recorder = NeuroscopeRecordingExtractor(file_path=lfp_file_path)
+
+        num_frames = recorder.get_num_frames()
+        sampling_frequency = recorder.get_sampling_frequency()
+        end_of_the_session = num_frames / sampling_frequency
+
         session_start = 0.0
         start_trials_time = min([interval[0] for interval in trial_interval_list])
         end_trials_time = max([interval[1] for interval in trial_interval_list])
-        end_of_the_session = timestamps[-1]
-        
-        nwbfile.add_epoch(start_time=session_start, stop_time=start_trials_time, tags='before trials')
-        nwbfile.add_epoch(start_time=start_trials_time, stop_time=end_trials_time, tags='during trials')
-        nwbfile.add_epoch(start_time=end_trials_time, stop_time=end_of_the_session, tags='after trials')
+        end_of_the_session = end_of_the_session
+
+        nwbfile.add_epoch(start_time=session_start, stop_time=start_trials_time, tags="before trials")
+        nwbfile.add_epoch(start_time=start_trials_time, stop_time=end_trials_time, tags="during trials")
+        nwbfile.add_epoch(start_time=end_trials_time, stop_time=end_of_the_session, tags="after trials")
