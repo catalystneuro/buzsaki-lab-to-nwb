11d32eba56d89c5b51acdd888f8a32575cf8471e
Cody Baker <cbaker9@nd.edu>
2022-04-16 12:35:24 -0400

    adding ordering utils

---
+++ buzsaki_lab_to_nwb/tingley_metabolic/tingley_metabolic_utils.py
@@ -0,0 +1,7 @@
+"""Author: Cody Baker."""
+from typing import List
+
+
+def order_sessions_by_start_time(session_names: List[str]):
+    """Given the list of {subject_id}_{start_time} strings, return a list ordered by the start_time."""
+    pass
