e9f884c2084916955f5a0cfd1ab203860bdf099d
Cody Baker <cbaker9@nd.edu>
2022-04-20 12:29:34 -0400

    final stages of debugging; falling back to simpler glucose representation

--- buzsaki_lab_to_nwb/tingley_metabolic/tingley_metabolic_utils.py
+++ buzsaki_lab_to_nwb/tingley_metabolic/tingley_metabolic_utils.py
@@ -2,56 +2,60 @@
 from typing import List, Optional
 from pathlib import Path
 from datetime import datetime
-from dataclasses import dataclass
+
+# from dataclasses import dataclass
 
 import numpy as np
 from pandas import read_csv, to_datetime
 
 
-@dataclass
-class GlucoseSeries:
-    # timestamps: Optional[List[datetime]] = None
-    # isig: Optional[List[float]] = None
+# @dataclass
+# class GlucoseSeries:
+#     # timestamps: Optional[List[datetime]] = None
+#     # isig: Optional[List[float]] = None
 
-    def __init__(self, timestamps: Optional[List[datetime]] = None, isig: Optional[List[float]] = None):
-        super().__init__()
-        self.timestamps = [] if timestamps is None else timestamps
-        self.isig = [] if isig is None else isig
-        self.order()
+#     def __init__(self, timestamps: Optional[List[datetime]] = None, isig: Optional[List[float]] = None):
+#         super().__init__()
+#         self.timestamps = [] if timestamps is None else timestamps
+#         self.isig = [] if isig is None else isig
+#         self.order()
 
-    def __add__(self, glucose_series):
-        self.timestamps.extend(glucose_series.timestamps)
-        self.isig.extend(glucose_series.isig)
-        self.order()
-        return self
+#     def __add__(self, glucose_series):
+#         self.timestamps.extend(glucose_series.timestamps)
+#         self.isig.extend(glucose_series.isig)
+#         self.order()
+#         return self
 
-    def order(self):
-        sorted_indices = np.argsort(self.timestamps)
-        # self.timestamps = list(np.array(self.timestamps)[sorted_indices])
-        unsorted_timestamps = list(self.timestamps)
-        self.timestamps = [unsorted_timestamps[idx] for idx in sorted_indices]
-        self.isig = list(np.array(self.isig)[sorted_indices])
+#     def order(self):
+#         sorted_indices = np.argsort(self.timestamps)
+#         # self.timestamps = list(np.array(self.timestamps)[sorted_indices])
+#         unsorted_timestamps = list(self.timestamps)
+#         self.timestamps = [unsorted_timestamps[idx] for idx in sorted_indices]
+#         self.isig = list(np.array(self.isig)[sorted_indices])
 
-    def subset(self, timestamp: datetime):
-        cutoff_idx = next(idx for idx, series_timestamp in enumerate(self.timestamps) if timestamp >= series_timestamp)
-        print(cutoff_idx)
-        timestamps = self.timestamps[:cutoff_idx]
-        isig = self.isig[:cutoff_idx]
-        return GlucoseSeries(timestamps=timestamps, isig=isig)
+#     def subset(self, timestamp: datetime):
+#         cutoff_idx = next(idx for idx, series_timestamp in enumerate(self.timestamps) if timestamp >= series_timestamp)
+#         print(cutoff_idx)
+#         timestamps = self.timestamps[:cutoff_idx]
+#         isig = self.isig[:cutoff_idx]
+#         return GlucoseSeries(timestamps=timestamps, isig=isig)
 
 
-def load_subject_glucose_series(session_path) -> GlucoseSeries:
+def load_subject_glucose_series(session_path) -> (List[datetime], List[float]):
     """Given the subject_id string and the ecephys session_path, load all glucose series data for further parsing."""
     subject_path = Path(session_path).parent
     all_csv = [x for x in subject_path.iterdir() if ".csv" in x.suffixes]
 
-    glucose_series = GlucoseSeries()
+    timestamps = []
+    isig = []
     for file_path in all_csv:
-        glucose_series += read_glucose_csv(file_path=file_path)
-    return glucose_series
+        sub_timestamps, sub_isig = read_glucose_csv(file_path=file_path)
+        timestamps.extend(sub_timestamps)
+        isig.extend(sub_isig)
+    return timestamps, isig
 
 
-def read_glucose_csv(file_path: Path) -> GlucoseSeries:
+def read_glucose_csv(file_path: Path) -> (List[datetime], List[float]):
     """Parse a single glucose data file."""
     all_data = read_csv(filepath_or_buffer=file_path, skiprows=11)
 
@@ -62,7 +66,7 @@ def read_glucose_csv(file_path: Path) -> GlucoseSeries:
         x.to_pydatetime() for x in to_datetime(all_data["Timestamp"][exclude], infer_datetime_format=True)
     ]
 
-    return GlucoseSeries(timestamps=valid_timestamps, isig=valid_isig)
+    return valid_timestamps, list(valid_isig)
 
 
 def get_session_datetime(session_id: str):
@@ -70,16 +74,16 @@ def get_session_datetime(session_id: str):
     return datetime.strptime("_".join(session_id.split("_")[-2:]), "%y%m%d_%H%M%S")
 
 
-def segment_glucose_series(
-    ecephys_start_time: datetime, ecephys_stop_time: datetime, glucose_series: GlucoseSeries
-) -> (GlucoseSeries, datetime):
-    """
-    Return either the entire glucose history or the subset leading to the end of this ecephys session.
+# def segment_glucose_series(
+#     ecephys_start_time: datetime, ecephys_stop_time: datetime, glucose_series: GlucoseSeries
+# ) -> (GlucoseSeries, datetime):
+#     """
+#     Return either the entire glucose history or the subset leading to the end of this ecephys session.
 
-    Also returns the NWB session start time.
-    """
-    # If glucose recording ended before this ecephys session
-    if ecephys_start_time > glucose_series.timestamps[-1]:
-        return glucose_series, ecephys_start_time
-    else:
-        return glucose_series.subset(timestamp=ecephys_stop_time), glucose_series.timestamps[0]
+#     Also returns the NWB session start time.
+#     """
+#     # If glucose recording ended before this ecephys session
+#     if ecephys_start_time > glucose_series.timestamps[-1]:
+#         return glucose_series, ecephys_start_time
+#     else:
+#         return glucose_series.subset(timestamp=ecephys_stop_time), glucose_series.timestamps[0]
--- buzsaki_lab_to_nwb/tingley_metabolic/tingleymetabolicglucoseinterface.py
+++ buzsaki_lab_to_nwb/tingley_metabolic/tingleymetabolicglucoseinterface.py
@@ -5,31 +5,34 @@ from nwb_conversion_tools.basedatainterface import BaseDataInterface
 from nwb_conversion_tools.utils import FilePathType
 from pynwb import TimeSeries, H5DataIO
 
-from .tingley_metabolic_utils import load_subject_glucose_series, segment_glucose_series
+from .tingley_metabolic_utils import load_subject_glucose_series  # , segment_glucose_series
 
 
 class TingleyMetabolicGlucoseInterface(BaseDataInterface):
     """Glucose data interface for the Tingley metabolic project."""
 
     def __init__(self, session_path: FilePathType, ecephys_start_time: str, ecephys_stop_time: str):
-        subject_glucose_series = load_subject_glucose_series(session_path=session_path)
-        session_glucose_series, session_start_time = segment_glucose_series(
-            ecephys_start_time=datetime.fromisoformat(ecephys_start_time),
-            ecephys_stop_time=datetime.fromisoformat(ecephys_stop_time),
-            glucose_series=subject_glucose_series,
-        )
-        self.session_start_time = session_start_time
-        self.glucose_series = session_glucose_series
+        glucose_timestamps, glucose_isig = load_subject_glucose_series(session_path=session_path)
+        # session_glucose_series, session_start_time = segment_glucose_series(
+        #     ecephys_start_time=datetime.fromisoformat(ecephys_start_time),
+        #     ecephys_stop_time=datetime.fromisoformat(ecephys_stop_time),
+        #     glucose_series=subject_glucose_series,
+        # )
+        self.session_start_time = glucose_timestamps[0]
+        glucose_timestamps_floats_from_datetime = [
+            (glucose_timestamp - self.session_start_time).total_seconds() for glucose_timestamp in glucose_timestamps
+        ]
+        self.glucose_timestamps = glucose_timestamps_floats_from_datetime
+        self.glucose_isig = glucose_isig
 
     def run_conversion(self, nwbfile, metadata):
-        print(self.glucose_series.isig)
         nwbfile.add_acquisition(
             TimeSeries(
                 name="GlucoseLevel",
                 description="Raw current from Medtronic iPro2 ISIG tracking.",
                 unit="nA",
-                data=H5DataIO(self.glucose_series.isig),  # should not need iterative write
+                data=H5DataIO(self.glucose_isig),  # should not need iterative write
                 conversion=1.0,
-                timestamps=H5DataIO(self.glucose_series.timestamps),
+                timestamps=H5DataIO(self.glucose_timestamps),
             ),
         )
