ca7ae070e9b9f22066385d109a2e38bdf887ee31
Heberto Mayorquin <h.mayorquin@gmail.com>
2023-05-24 10:10:25 +0200

    Add HSE events interface/home/heberto/miniconda3/envs/neuroconv_env/bin/python

--- buzsaki_lab_to_nwb/valero/convert_session.py
+++ buzsaki_lab_to_nwb/valero/convert_session.py
@@ -71,6 +71,10 @@ def session_to_nwb(session_dir_path, output_dir_path, stub_test=False, verbose=F
     folder_path = session_dir_path
     source_data.update(HSEvents=dict(folder_path=str(folder_path)))
 
+    # Add UP and Down events
+    folder_path = session_dir_path
+    source_data.update(UPDownEvents=dict(folder_path=str(folder_path)))
+
     # Add sleep states
     folder_path = session_dir_path
     source_data.update(BehaviorSleepStates=dict(folder_path=str(folder_path)))
