7b2049dc72eb65d1dd07e79af895d4c607ece7e0
Cody Baker <cody.c.baker.phd@gmail.com>
2023-06-15 19:05:28 +0000

    Automated changes

--- buzsaki_lab_to_nwb/huszar_hippocampus_dynamics/trialsinterface.py
+++ buzsaki_lab_to_nwb/huszar_hippocampus_dynamics/trialsinterface.py
@@ -50,15 +50,25 @@ class HuszarTrialsInterface(BaseDataInterface):
 
         [nwbfile.add_trial(**row) for row in sorted(data, key=lambda x: x["start_time"])]
 
-        nwbfile.add_trial_column(name="choice", description="An value of 0 or 1 representing whether the expected and visited arm of the trial matches", data=trial_info["choice"])
         nwbfile.add_trial_column(
-            name="visited_arm", description="An integer value representing the visited arm of the trial", data=trial_info["visitedArm"]
+            name="choice",
+            description="An value of 0 or 1 representing whether the expected and visited arm of the trial matches",
+            data=trial_info["choice"],
         )
         nwbfile.add_trial_column(
-            name="expected_arm", description="An integer value representing the expected arm of the trial", data=trial_info["expectedArm"]
+            name="visited_arm",
+            description="An integer value representing the visited arm of the trial",
+            data=trial_info["visitedArm"],
         )
         nwbfile.add_trial_column(
-            name="recordings", description="An integer value representing which recording this trial belongs to", data=trial_info["recordings"]
+            name="expected_arm",
+            description="An integer value representing the expected arm of the trial",
+            data=trial_info["expectedArm"],
+        )
+        nwbfile.add_trial_column(
+            name="recordings",
+            description="An integer value representing which recording this trial belongs to",
+            data=trial_info["recordings"],
         )
         # nwbfile.add_trial_column(name="start_point", description="start point of the trial", data=trial_info['startPoint'])
         # nwbfile.add_trial_column(name="end_delay", description="end delay of the trial", data=trial_info['endDelay'])
