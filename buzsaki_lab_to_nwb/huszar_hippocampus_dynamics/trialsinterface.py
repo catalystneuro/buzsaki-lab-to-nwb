a853dc8ed5934a81bb4047a59639fb1f78911b49
Cody Baker <cody.c.baker.phd@gmail.com>
2023-06-26 16:24:40 +0000

    Automated changes

--- buzsaki_lab_to_nwb/huszar_hippocampus_dynamics/trialsinterface.py
+++ buzsaki_lab_to_nwb/huszar_hippocampus_dynamics/trialsinterface.py
@@ -26,22 +26,21 @@ def access_behavior_property_safe(property, parent, behavior_mat):
 def isNaN(num):
     return num != num
 
+
 def to_direction(arr):
     output = []
     for value in arr:
+        if isNaN(value):
+            output.append("timeout")
 
-        if (isNaN(value)):
-            output.append('timeout')
-        
-        elif (value == 0):
-            output.append('right')
+        elif value == 0:
+            output.append("right")
 
         else:
-            output.append('left')
+            output.append("left")
 
     return output
 
-        
 
 # Add trial table from the behavior file
 class HuszarTrialsInterface(BaseDataInterface):
@@ -78,7 +77,6 @@ class HuszarTrialsInterface(BaseDataInterface):
 
         [nwbfile.add_trial(**row) for row in sorted(data, key=lambda x: x["start_time"])]
 
-
         visited_arm_data = to_direction(trial_info["visitedArm"])
         expected_arm_data = to_direction(trial_info["expectedArm"])
 
@@ -91,13 +89,13 @@ class HuszarTrialsInterface(BaseDataInterface):
         nwbfile.add_trial_column(
             name="visited_arm",
             description="An integer value representing the visited arm of the trial",
-            data=H5DataIO(visited_arm_data, compression="gzip") 
+            data=H5DataIO(visited_arm_data, compression="gzip"),
         )
 
         nwbfile.add_trial_column(
             name="expected_arm",
             description="An integer value representing the expected arm of the trial",
-            data=H5DataIO(expected_arm_data, compression="gzip") 
+            data=H5DataIO(expected_arm_data, compression="gzip"),
         )
 
         nwbfile.add_trial_column(
